dea3297f116c34ba40b6b5bc3dafe1b77ee980d2
SaiVenkat12 <gsaiv1001@gmail.com>
2023-01-23 09:26:11 +0530

    [SaiVenkat] Calculating Heads and Tails Percentage

---
+++ BasicCorePrograms/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Basic Core Programs");
+            Console.WriteLine("Enter the number of times you want to Toss the Coin: ");
+            int n=Convert.ToInt32(Console.ReadLine());
+            HeadsAndTails.HeadandTailPercentage(n);
+            Console.ReadLine();
+        }
+    }
+}
