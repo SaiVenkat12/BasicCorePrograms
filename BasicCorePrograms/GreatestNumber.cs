eb483f69cab4921cde5d1a2718e996adc145314f
SaiVenkat12 <gsaiv1001@gmail.com>
2023-01-26 22:26:24 +0530

    [SaiVenkat] Added Greatest among 3 Numbers

---
+++ BasicCorePrograms/GreatestNumber.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    internal class GreatestNumber
+    {
+        public static void GreatestAmong3()
+        {
+            Console.WriteLine("Enter the First Number: ");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Second Number: ");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Third Number: ");
+            int thirdNumber = Convert.ToInt32(Console.ReadLine());
+            if ((firstNumber > secondNumber) && (firstNumber > thirdNumber))
+            {
+                Console.WriteLine("The Greatest Number is " + firstNumber);
+            }
+            else if((secondNumber > firstNumber) && (secondNumber > thirdNumber))
+            {
+                Console.WriteLine("The Greatest Number is " + secondNumber);
+            }
+            else
+            {
+                Console.WriteLine("The Greatest Number is " + thirdNumber);
+            }
+        }
+    }
+}
--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -15,7 +15,7 @@ namespace BasicCorePrograms
             bool flag = true;
             while(flag)
             {
-                Console.WriteLine("1.Heads and Tails \n2.Leap Year or not \n3.Power of 2 \n4.Harmonic Number \n5.Swap 2 Numbers \n6.Prime Factors of a Number \n7.Quotient And Remainder \n8.Even or Odd \n9.Vowel or Consonant \n11.Exit");
+                Console.WriteLine("1.Heads and Tails \n2.Leap Year or not \n3.Power of 2 \n4.Harmonic Number \n5.Swap 2 Numbers \n6.Prime Factors of a Number \n7.Quotient And Remainder \n8.Even or Odd \n9.Vowel or Consonant \n10.Greatest Among 3 Numbers \n11.Exit");
                 int choice = Convert.ToInt32(Console.ReadLine());
                 switch (choice)
                 {
@@ -45,6 +45,9 @@ namespace BasicCorePrograms
                         break;
                     case 9:
                         VowelOrConsonant.Vowel();
+                        break;
+                    case 10:
+                        GreatestNumber.GreatestAmong3();
                         break;
                     case 11:
                         flag= false;
