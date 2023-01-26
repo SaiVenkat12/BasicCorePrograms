adcb493e64c202a58bbfb1caec57e01d5a631bc9
SaiVenkat12 <gsaiv1001@gmail.com>
2023-01-26 21:34:26 +0530

    [SaiVenkat] Added Quotient and Remainder after Division

--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -15,7 +15,7 @@ namespace BasicCorePrograms
             bool flag = true;
             while(flag)
             {
-                Console.WriteLine("1.Heads and Tails \n2.Leap Year or not \n3.Power of 2 \n4.Harmonic Number \n5.Swap 2 Numbers \n6.Prime Factors of a Number \n11.Exit");
+                Console.WriteLine("1.Heads and Tails \n2.Leap Year or not \n3.Power of 2 \n4.Harmonic Number \n5.Swap 2 Numbers \n6.Prime Factors of a Number \n7.Quotient And Remainder \n11.Exit");
                 int choice = Convert.ToInt32(Console.ReadLine());
                 switch (choice)
                 {
@@ -36,6 +36,9 @@ namespace BasicCorePrograms
                         break;
                     case 6:
                         Factors.PrimeFactors();
+                        break;
+                    case 7:
+                        QuotientAndRemainder.Division();
                         break;
                     case 11:
                         flag= false;
---
+++ BasicCorePrograms/QuotientAndRemainder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    internal class QuotientAndRemainder
+    {
+        public static void Division()
+        {
+            int remainder, quotient;
+            Console.WriteLine("Enter the Number : ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Divisor : ");
+            int d = Convert.ToInt32(Console.ReadLine());
+            remainder = n % d;
+            quotient = n / d;
+            Console.WriteLine("Quotient = {0}\nRemainder = {1}", quotient, remainder);
+
+        }
+    }
+}
